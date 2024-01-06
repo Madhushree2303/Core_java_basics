f413554cc7bde1b5d6bb98a6ddedf93edbdb24b2
Madhushree2303 <madhu.balan023@gmail.com>
2024-01-06 22:25:54 +0530

    new commit

---
+++ Java_basics_04/Java_concept_33.java
@@ -0,0 +1,30 @@
+package Java_basics_04;
+public class Java_concept_33 {
+    public static void main(String[] args) {
+
+        // for( A ;B ; C)
+        // A -> Initialization -> JVM What variable, where you are starting point
+        // B - Condition -> When you want this to stop.
+        // C ->  Increment / Decrement
+
+        // this will run until we stop
+       /* for (;;){
+            System.out.println("madhu");
+        } */
+
+        // if condition false then code inside for loop will not exit
+       /* boolean b=false;
+        for (;b;){
+            System.out.println("test user");
+        }*/
+
+        // if condition false then code inside for loop will not exit
+        final boolean b1 = true;
+        //b1=false;
+        for (float f = 0; b1; )
+            System.out.println("TTA");
+
+
+    }
+}
+
